4bba2ce3a15caca905242f68d9423188f3844b11
PrabulPRK <99730122+PrabulPRK@users.noreply.github.com>
2022-07-30 12:55:21 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const getElement = (selector) => {
-  const element = document.querySelector(selector)
+// const getElement = (selector) => {
+//   const element = document.querySelector(selector)
 
-  if (element) return element
-  throw Error(
-    `Please double check your class names, there is no ${selector} class`
-  )
-}
+//   if (element) return element
+//   throw Error(
+//     `Please double check your class names, there is no ${selector} class`
+//   )
+// }
 
 const links = getElement('.nav-links')
 const navBtnDOM = getElement('.nav-btn')
