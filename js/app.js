4e2d106cf7bfd02dae54f21f30ba6a2d8eafff07
PrabulPRK <99730122+PrabulPRK@users.noreply.github.com>
2022-07-30 12:57:36 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@
 //   )
 // }
 
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
+// const links = getElement('.nav-links')
+// const navBtnDOM = getElement('.nav-btn')
 
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
+// navBtnDOM.addEventListener('click', () => {
+//   links.classList.toggle('show-links')
+// })
 
 const date = getElement('#date')
 const currentYear = new Date().getFullYear()
